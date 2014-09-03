13ed002f103af65efa6169cb73af0f98afb8ab97
Andrew House <andrewhouse621@gmail.com>
2014-09-03 13:44:55 -0400

    sort for homework assignment form

--- app/assets/javascripts/tooltip.js
+++ app/assets/javascripts/tooltip.js
@@ -1,10 +1,22 @@
-function documentLoad(){
+function jqToolTip(){
   $('.ass').tooltip();
+}
+
+function jqDatePicker(){
   $(".jquery-ui-date").datepicker({
     altField: "#recorded-at-alt",
     altFormat: "yy-mm-d"
   });
+}
+
+function jqAccordion(){
   $('#accordion').accordion({active: false, collapsible: true});
+}
+
+function documentLoad(){
+  jqToolTip();
+  jqDatePicker();
+  jqAccordion();
 }
 
 $(document).ready(documentLoad);
