b9f7f1871349d5c8187037508d6bee419861a1f2
Andrew House <andrewhouse621@gmail.com>
2014-09-05 09:53:40 -0400

    upgrade to angular 1.3 and add debounce for search

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -10,7 +10,7 @@ angular.module('table', ['ngResource']).
   var options2 = {weekday: "narrow", month: "2-digit", day: "numeric"}
 
   $scope.date_filter = function(assignment) {
-    if (!$scope.search) { return false; } 
+    if (!$scope.search) { return false; }
     lowercase = $scope.search.toLowerCase();
     title = assignment.title.toLowerCase();
     description = assignment.description.toLowerCase();
@@ -39,4 +39,31 @@ angular.module('table', ['ngResource']).
       $('.ass').tooltip();
     }
   }
-});
+}).directive('ngDebounce', function($timeout) {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        priority: 99,
+        link: function(scope, elm, attr, ngModelCtrl) {
+            if (attr.type === 'radio' || attr.type === 'checkbox') return;
+
+            elm.unbind('input');
+
+            var debounce;
+            elm.bind('input', function() {
+                $timeout.cancel(debounce);
+                debounce = $timeout( function() {
+                    scope.$apply(function() {
+                        ngModelCtrl.$setViewValue(elm.val());
+                    });
+                }, attr.ngDebounce || 1000);
+            });
+            elm.bind('blur', function() {
+                scope.$apply(function() {
+                    ngModelCtrl.$setViewValue(elm.val());
+                });
+            });
+        }
+
+    }
+});;
